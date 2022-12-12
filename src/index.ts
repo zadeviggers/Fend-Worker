13986fd0072ecf46f6cc17530ad728573f841975
Zade Viggers <zade@viggers.net>
2022-12-12 16:44:20 +1300

    Load wasm earlier to give more time for execution

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 
 const timeout = 8; // Limit execution to 8ms since deno deploy has 10ms limit for total execution time.
 
-serve(async (request: Request) => {
+await initialise();
+
+serve((request: Request) => {
   const query = new URL(request.url).searchParams.get("q");
   if (!query) {
     return new Response("Add a Fend query in the `q` query parameter.");
   }
   try {
-    await initialise();
     const result = evaluateFendWithTimeout(query, timeout);
     return new Response(result);
   } catch (e) {
