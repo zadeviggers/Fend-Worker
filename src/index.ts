76e6859dfefcf8f40cb05efdb737c9e7908cf04a
Zade Viggers <zade@viggers.net>
2022-12-12 17:39:06 +1300

    Add support for suggestions

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ const timeout = 8; // Limit execution to 8ms since deno deploy has 10ms limit fo
 await initialise();
 
 serve((request: Request) => {
-  const query = new URL(request.url).searchParams.get("q");
+  const url = new URL(request.url);
+  const query = url.searchParams.get("q");
   if (!query) {
     return new Response("Add a Fend query in the `q` query parameter.");
   }
@@ -20,6 +21,14 @@ serve((request: Request) => {
       );
     else if (result.startsWith("Error"))
       return new Response(result, { status: 500 });
+
+    // OpenSearch suggestions
+    // Read more here: https://udn.realityripple.com/docs/Archive/Add-ons/Supporting_search_suggestions_in_search_plugins#Implementing_search_suggestion_support_on_the_server
+    if (url.pathname.startsWith("/suggestions"))
+      return new Response(JSON.stringify([query, [`= ${result}`]]), {
+        headers: { "Content-Type": "application/x-suggestions+json" },
+      });
+
     return new Response(result);
   } catch (e) {
     console.error(e);
